8eb963ba30d174956e04be4d6130dee81b5dd218
Luigi8189 <luigi8939@gmail.com>
2025-07-22 11:49:17 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Animale animaleGenerico = new Animale();
         Gatto gatto = new Gatto();
+        Animale animaleGenerico = new Animale();
+        gatto.faiIlVerso();
+        animaleGenerico.faiIlVerso();
+    }
+}
 
-        animaleGenerico.faiIlVerso();  // Output: L'animale fa un verso generico.
-        gatto.faiIlVerso();            // Output: Miao!
+class Gatto extends Animale {
+    public void faiIlVerso() {
+        System.out.println("Miao!");
+    }
+}
+
+class Animale {
+    public void faiIlVerso() {
+        System.out.println("L'animale fa un verso generico.");
     }
 }
